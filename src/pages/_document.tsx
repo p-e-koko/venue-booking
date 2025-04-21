96b8af729f759bb6f2ec0311c3165bd6fb01c4ff
p-e-koko <panneikoko1221@gmail.com>
2025-04-21 22:02:06 +0700

    change: styling

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
