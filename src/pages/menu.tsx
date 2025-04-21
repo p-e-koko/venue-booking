03e7b8f03493fab3c0eeefd273533d4e9bf42e8f
p-e-koko <panneikoko1221@gmail.com>
2025-04-21 22:01:02 +0700

    add: menu page

---
+++ src/pages/menu.tsx
@@ -0,0 +1,11 @@
+export default function Menu() {
+    return (
+        <section>
+            <h1>Choose your options</h1>
+            <ul>
+                <li>Book venue</li>
+                <li>Manage booking</li>
+            </ul>
+        </section>
+    );
+}
